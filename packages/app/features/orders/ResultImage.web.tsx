31cf47e977e7ccd7fd338b108832a7658cb39d99
Devin Gould <djgould0628@gmail.com>
2023-07-03 11:01:04 -0600

    ship

--- packages/app/features/orders/ResultImage.web.tsx
+++ packages/app/features/orders/ResultImage.web.tsx
@@ -6,26 +6,6 @@ export interface Props {
 }
 
 export default function ResultImage({ url }: Props) {
-  const [watermarkUrl, setWatermarkUrl] = useState<string | null>(null)
-  useEffect(() => {
-    if (typeof window === 'undefined') return
-    const run = async () => {
-      const watermark = (await import('watermarkjs')).default
-      try {
-        watermark(url)
-          .dataUrl(watermark.text.lowerLeft('watermark.js', '48px Josefin Slab', '#fff', 0.5))
-          .then((dataUrl) => {
-            console.log(dataUrl)
-            setWatermarkUrl(dataUrl)
-          })
-      } catch (e) {
-        console.error(e)
-      }
-    }
-    run()
-  }, [url])
-  console.log(watermarkUrl)
-  if (!watermarkUrl) return null
   return (
     <Square
       backgroundColor={'$backgroundStrong'}
@@ -34,7 +14,7 @@ export default function ResultImage({ url }: Props) {
       aspectRatio={1}
       position={'relative'}
     >
-      <Image position="relative" source={{ uri: watermarkUrl }} width="100%" height="100%" />
+      <Image position="relative" source={{ uri: url }} width="100%" height="100%" />
     </Square>
   )
 }
